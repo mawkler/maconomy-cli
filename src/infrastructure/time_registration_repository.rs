7b12ce1c1c64a6aea91ab5cc424c990f54727505
Melker Ulander <melker.ulander@pm.me>
2024-09-29 12:45:54 +0200

    Refactor out fetch_container_instance

--- src/infrastructure/time_registration_repository.rs
+++ src/infrastructure/time_registration_repository.rs
@@ -7,6 +7,7 @@ use serde::Deserialize;
 
 const MACONOMY_CONTAINERS_JSON: &str = "application/vnd.deltek.maconomy.containers+json";
 
+#[derive(Clone)]
 struct ContainerInstance {
     id: String,
     /// Needs to be included in a Maconomy-Concurrency-Control header for each request to Maconomy
@@ -51,7 +52,7 @@ impl TimeRegistrationRepository {
         })
     }
 
-    async fn get_container_instance(&self) -> Result<ContainerInstance> {
+    async fn fetch_container_instance(&self) -> Result<ContainerInstance> {
         let (url, company) = (&self.url, &self.company_name);
         let url = format!("{url}/containers/{company}/timeregistration/instances");
         let body = include_str!("request_bodies/time_registration_container.json");
@@ -90,34 +91,36 @@ impl TimeRegistrationRepository {
         })
     }
 
-    pub async fn get_time_registration(&mut self) -> Result<TimeRegistration> {
+    async fn get_container_instance(&mut self) -> Result<ContainerInstance> {
         if self.container_instance.is_none() {
             info!("Fetching container instance");
             let container_instance = self
-                .get_container_instance()
+                .fetch_container_instance()
                 .await
                 .context("Failed to get container instance ID")?;
 
             self.container_instance = Some(container_instance);
         }
 
-        let container_instance = self
-            .container_instance
-            .as_ref()
-            .expect("Missing container instance");
-        let instance_id = &container_instance.id;
+        let container_instance = self.container_instance.as_ref().ok_or(anyhow!(
+            "Missing container instance even though we just fetched it"
+        ))?;
+        Ok(container_instance.clone())
+    }
+
+    pub async fn get_time_registration(&mut self) -> Result<TimeRegistration> {
+        let ContainerInstance {
+            id,
+            concurrency_control,
+        } = self.get_container_instance().await?;
 
         let (url, company) = (&self.url, &self.company_name);
-        let url =
-            format!("{url}/containers/{company}/timeregistration/instances/{instance_id}/data;any");
+        let url = format!("{url}/containers/{company}/timeregistration/instances/{id}/data;any");
 
         let request = self
             .client
             .post(url)
-            .header(
-                "Maconomy-Concurrency-Control",
-                &container_instance.concurrency_control,
-            )
+            .header("Maconomy-Concurrency-Control", concurrency_control)
             .header("Content-length", "0");
 
         let response = self
