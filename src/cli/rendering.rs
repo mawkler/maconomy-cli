70172f5c9b6c3cecd5fe80c116f9f3ec1564e1d7
Melker Ulander <melker.ulander@pm.me>
2024-10-01 16:24:01 +0200

    Add ANSI coloring to table rendering

--- src/cli/rendering.rs
+++ src/cli/rendering.rs
@@ -1,6 +1,9 @@
 use crate::infrastructure::time_registration;
+use owo_colors::OwoColorize;
 use std::fmt::Display;
-use tabled::settings::{object::Rows, themes::Colorization, Color, Format, Style, Theme};
+use tabled::settings::{
+    object::Rows, style::BorderColor, themes::Colorization, Color, Style, Theme,
+};
 
 #[derive(tabled::Tabled, Default)]
 pub(crate) struct Row<'a> {
@@ -40,21 +43,37 @@ impl<'a> From<&'a time_registration::TableRecord> for Row<'a> {
     }
 }
 
+fn gray_borders() -> BorderColor {
+    let gray = Color::parse(' '.fg_rgb::<85, 85, 85>().to_string()).clone();
+
+    BorderColor::new()
+        .top(gray.clone())
+        .left(gray.clone())
+        .bottom(gray.clone())
+        .right(gray.clone())
+        .corner_bottom_right(gray.clone())
+        .corner_bottom_left(gray.clone())
+        .corner_top_left(gray.clone())
+        .corner_top_right(gray.clone())
+}
+
 impl Display for time_registration::TimeRegistration {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let rows = self.panes.table.records.iter().map(Row::from);
-        let mut table = tabled::Table::new(rows);
-
-        let table = table.with(Colorization::exact(
-            [tabled::settings::Color::BOLD],
-            Rows::first(),
-        ));
 
         let mut theme = Theme::from_style(Style::modern_rounded());
         theme.remove_vertical_lines();
 
-        let table = table.with(theme).to_string();
+        let mut table = tabled::Table::new(rows);
+        let table = table
+            .with(theme)
+            .with(BorderColor::new().top(Color::FG_GREEN))
+            .with(Colorization::exact(
+                [tabled::settings::Color::BOLD],
+                Rows::first(),
+            ))
+            .with(gray_borders());
 
-        write!(f, "{}", table)
+        write!(f, "{table}")
     }
 }
